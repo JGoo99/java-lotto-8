6a89efa5f02ce1e52927e2dc25c93f5970b7a6ac
JinGoo <zzgg3040@naver.com>
2025-11-03 02:06:45 +0900

    feat: 금액이 1,000원 단위가 아니면 예외가 발생한다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,52 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            new Application().run();
+        } finally {
+            Console.close();
+        }
+    }
+
+    void run() {
+        int amount = readPurchaseAmount();
+    }
+
+    private int readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return retry(() -> {
+            String s = Console.readLine();
+            int money = parseIntStrict(s);
+            if (money <= 0 || money % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위의 양수여야 합니다.");
+            }
+            return money;
+        });
+    }
+
+    private <T> T retry(SupplierWithEx<T> sup) {
+        while (true) {
+            try {
+                return sup.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static int parseIntStrict(String s) {
+        try {
+            return Integer.parseInt(s.trim());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+        }
+    }
+
+    @FunctionalInterface
+    interface SupplierWithEx<T> {
+        T get();
     }
 }
